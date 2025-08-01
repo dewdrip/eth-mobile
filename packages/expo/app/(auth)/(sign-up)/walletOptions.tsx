1d1fcc4caf4127ecd505ef43e6f3259d7c48f48f
Valentine Orga <valentineorga@gmail.com>
2025-08-01 21:21:47 +0100

    feat: design screen to create or import wallet

--- packages/expo/app/(auth)/(sign-up)/walletOptions.tsx
+++ packages/expo/app/(auth)/(sign-up)/walletOptions.tsx
@@ -1,10 +1,70 @@
+import BackButton from '@/components/buttons/BackButton';
+import Button from '@/components/buttons/CustomButton';
+import { COLORS } from '@/utils/constants';
+import { useRouter } from 'expo-router';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, SafeAreaView, ScrollView, Text, View } from 'react-native';
 
 export default function WalletOptions() {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Wallet Options</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <ScrollView
+        contentContainerStyle={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <View className="w-full aspect-square">
+          <Image
+            source={require('../../../assets/images/work_bg.png')}
+            style={{
+              width: '100%',
+              height: '100%'
+            }}
+            resizeMode="cover"
+          />
+
+          <View className="absolute bottom-0 right-0">
+            <Image
+              source={require('../../../assets/images/thumbs_up.png')}
+              resizeMode="contain"
+            />
+          </View>
+        </View>
+
+        <BackButton
+          style={{
+            position: 'absolute',
+            top: 10,
+            left: 15
+          }}
+        />
+
+        <View className="w-full mt-10 px-5">
+          <Text
+            className="text-4xl font-[Poppins]"
+            style={{ color: COLORS.primary }}
+          >
+            Wallet Setup
+          </Text>
+          <Text className="text-gray-500 text-base font-[Poppins] mt-2 my-4">
+            Create your new Wallet or import using a seed phrase if you already
+            have an account
+          </Text>
+
+          <View className="w-full gap-y-2">
+            <Button text="Create a New Wallet" onPress={() => {}} />
+
+            <Button
+              type="outline"
+              text="Import Using Seed Phrase"
+              onPress={() => {}}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }

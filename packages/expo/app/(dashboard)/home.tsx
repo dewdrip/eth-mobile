d4c1859c56d07e31345faf5e9e9ae10e0d6f664f
Valentine Orga <valentineorga@gmail.com>
2025-10-03 17:23:07 +0100

    feat: integrate mopro bindings for android and ios

--- packages/expo/app/(dashboard)/home.tsx
+++ packages/expo/app/(dashboard)/home.tsx
@@ -1,100 +1,240 @@
-import Device from '@/utils/device';
-import { Ionicons } from '@expo/vector-icons';
-import { Link } from 'expo-router';
-import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+import {
+  generateNoirProof,
+  getNoirVerificationKey,
+  verifyNoirProof
+} from '@/modules/mopro';
+import * as FileSystem from 'expo-file-system';
+import { useState } from 'react';
+import {
+  Button,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
 
-function HighlightedText({ children }: { children: string }) {
+function NoirProofComponent() {
+  const [a, setA] = useState('3');
+  const [b, setB] = useState('4');
+  const [inputs, setInputs] = useState<string[]>([]);
+  const [proof, setProof] = useState<Uint8Array>(new Uint8Array());
+  const [isValid, setIsValid] = useState<string>('');
+  const [vk, setVk] = useState<Uint8Array>(new Uint8Array());
+
+  async function genProof(): Promise<void> {
+    const circuitInputs = [a, b];
+    if (Platform.OS === 'web') {
+      console.log('not implemented');
+    } else if (Platform.OS === 'android' || Platform.OS === 'ios') {
+      const circuitName = 'noir_multiplier2.json';
+
+      const content = require(`@/assets/keys/${circuitName}`);
+
+      const newFilePath = `${FileSystem.documentDirectory}${circuitName}`;
+
+      const fileInfo = await FileSystem.getInfoAsync(newFilePath);
+      if (!fileInfo.exists) {
+        try {
+          await FileSystem.writeAsStringAsync(
+            newFilePath,
+            JSON.stringify(content)
+          );
+        } catch (error) {
+          console.error('Error copying file:', error);
+          throw error;
+        }
+      }
+
+      try {
+        const onChain = true;
+        const lowMemoryMode = false;
+        let verificationKey: Uint8Array;
+        if (vk.length === 0) {
+          verificationKey = await getNoirVerificationKey(
+            newFilePath.replace('file://', ''),
+            null,
+            onChain,
+            lowMemoryMode
+          );
+          setVk(verificationKey);
+        } else {
+          verificationKey = vk;
+        }
+
+        const res: Uint8Array = await generateNoirProof(
+          newFilePath.replace('file://', ''),
+          null,
+          circuitInputs,
+          onChain,
+          verificationKey,
+          lowMemoryMode
+        );
+        setProof(res);
+      } catch (error) {
+        console.error('Error generating proof:', error);
+      }
+    }
+  }
+
+  async function verifyProof(): Promise<void> {
+    if (Platform.OS === 'web') {
+      setIsValid('not implemented');
+    } else if (Platform.OS === 'android' || Platform.OS === 'ios') {
+      if (proof.length === 0) {
+        setIsValid('Error: Proof data is not available. Generate proof first.');
+        return;
+      }
+
+      if (vk.length === 0) {
+        setIsValid(
+          'Error: Verification key is not available. Generate proof first.'
+        );
+        return;
+      }
+
+      const circuitName = 'noir_multiplier2.json';
+
+      const content = require(`@/assets/keys/${circuitName}`);
+
+      const newFilePath = `${FileSystem.documentDirectory}${circuitName}`;
+
+      const fileInfo = await FileSystem.getInfoAsync(newFilePath);
+      if (!fileInfo.exists) {
+        try {
+          await FileSystem.writeAsStringAsync(
+            newFilePath,
+            JSON.stringify(content)
+          );
+        } catch (error) {
+          console.error('Error copying file:', error);
+          throw error;
+        }
+      }
+
+      try {
+        const onChain = true;
+        const lowMemoryMode = false;
+
+        const res: boolean = await verifyNoirProof(
+          newFilePath.replace('file://', ''),
+          proof,
+          onChain,
+          vk,
+          lowMemoryMode
+        );
+        setIsValid(res.toString());
+      } catch (error) {
+        console.error('Error verifying proof:', error);
+      }
+    }
+  }
+
   return (
-    <View className="bg-green-100 py-1 rounded-full px-4">
-      <Text className="text-center text-lg font-[Poppins]">{children}</Text>
+    <View style={styles.proofContainer}>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>a</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter value for a"
+          value={a}
+          onChangeText={setA}
+          keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>b</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter value for b"
+          value={b}
+          onChangeText={setB}
+          keyboardType="numeric"
+        />
+      </View>
+      <Button title="Generate Noir Proof" onPress={() => genProof()} />
+      <Button title="Verify Noir Proof" onPress={() => verifyProof()} />
+      <View style={styles.stepContainer}>
+        <Text style={{ fontWeight: '600' }}>Proof is Valid:</Text>
+        <Text style={styles.output}>{isValid}</Text>
+        <Text style={{ fontWeight: '600' }}>Proof:</Text>
+        <ScrollView style={styles.outputScroll}>
+          <Text style={styles.output}>{proof}</Text>
+        </ScrollView>
+      </View>
     </View>
   );
 }
 
 export default function Home() {
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View className="flex flex-row items-center justify-between px-4 py-2">
-          <Text className="text-2xl font-bold font-[Poppins-Bold]">
-            ETH Mobile
-          </Text>
-
-          <View className="flex flex-row items-center gap-x-5">
-            <Link href="/debugContracts" push asChild>
-              <Ionicons
-                name="bug-outline"
-                size={Device.getDeviceWidth() * 0.07}
-                color="#555"
-              />
-            </Link>
-
-            <Link href="/wallet" push asChild>
-              <Ionicons
-                name="wallet-outline"
-                size={Device.getDeviceWidth() * 0.07}
-                color="#555"
-              />
-            </Link>
-
-            <Link href="/settings" push asChild>
-              <Ionicons
-                name="settings-outline"
-                size={Device.getDeviceWidth() * 0.07}
-                color="#555"
-              />
-            </Link>
-          </View>
-        </View>
-
-        <View className="items-center p-8 gap-y-2">
-          <Text className="text-3xl text-center mb-4 font-[Poppins]">
-            The Building Blocks of Mobile dApps
-          </Text>
-
-          <Text className="text-xl font-[Poppins]">Get started by editing</Text>
-          <HighlightedText>packages/expo/app</HighlightedText>
-
-          <View className="flex-row items-center gap-x-1 max-w-full mt-2">
-            <Text className="text-lg font-[Poppins]">
-              Edit your smart contract in
-            </Text>
-          </View>
-          <View className="flex-row items-center gap-x-1 max-w-full">
-            <HighlightedText>packages/hardhat/contracts</HighlightedText>
-          </View>
-        </View>
-
-        <View className="flex-1 pb-4 justify-center items-center gap-4">
-          <Link href="/debugContracts" className="w-[90%]" push>
-            <View className="items-center py-8 border w-full border-gray-300 rounded-3xl bg-white gap-4">
-              <Ionicons
-                name="bug-outline"
-                color="grey"
-                size={Device.getDeviceWidth() * 0.09}
-              />
-
-              <Text className="text-center text-xl w-[60%] font-[Poppins]">
-                Tinker with your smart contracts
-              </Text>
-            </View>
-          </Link>
-
-          <Link href="/wallet" className="w-[90%]" push>
-            <View className="items-center py-8 border w-full border-gray-300 rounded-3xl bg-white gap-4">
-              <Ionicons
-                name="wallet-outline"
-                color="grey"
-                size={Device.getDeviceWidth() * 0.09}
-              />
-
-              <Text className="text-center text-xl w-[60%] font-[Poppins]">
-                Manage your accounts, funds, and tokens
-              </Text>
-            </View>
-          </Link>
-        </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+      <ScrollView contentContainerStyle={{ padding: 16 }}>
+        <NoirProofComponent />
       </ScrollView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    flex: 1,
+    paddingHorizontal: 10
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  label: {
+    fontSize: 16,
+    marginRight: 10
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute'
+  },
+  outputScroll: {
+    maxHeight: 150,
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginBottom: 10
+  },
+  output: {
+    fontSize: 14,
+    padding: 10
+  },
+  tabContainer: {
+    flexDirection: 'row',
+    marginBottom: 20
+  },
+  tab: {
+    flex: 1,
+    padding: 15,
+    alignItems: 'center',
+    borderBottomWidth: 2,
+    borderBottomColor: '#ccc'
+  },
+  activeTab: {
+    borderBottomColor: '#A1CEDC'
+  },
+  tabText: {
+    fontSize: 16,
+    fontWeight: '500'
+  },
+  proofContainer: {
+    padding: 10
+  }
+});
---
+++ packages/expo/modules/mopro/src/MoproView.web.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { MoproViewProps } from './Mopro.types';
+
+export default function MoproView(props: MoproViewProps) {
+  return (
+    <div>
+      <span>{props.name}</span>
+    </div>
+  );
+}

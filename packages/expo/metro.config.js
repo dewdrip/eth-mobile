097dd2f1a1cd171169a98a18e09032eeeb3be9e0
Valentine Orga <valentineorga@gmail.com>
2025-10-11 12:16:59 +0100

    chore: configure metro to support zkey files and other wasm file types

--- packages/expo/metro.config.js
+++ packages/expo/metro.config.js
@@ -1,8 +1,17 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
+
+// Add support for .zkey files and other custom asset types
+config.resolver.assetExts.push('zkey', 'wasm', 'bin');
+
+// Configure path alias resolution
+config.resolver.alias = {
+  '@': path.resolve(__dirname, './')
+};
 
 module.exports = withNativeWind(config, {
   input: './global.css',
